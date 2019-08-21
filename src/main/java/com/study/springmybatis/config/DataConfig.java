1a0e519e059279e3bb004b4fa8710e7c403f144a
MrMingFei <18330368050@163.com>
2019-08-21 17:29:34 +0800

    19/8/21

--- src/main/java/com/study/springmybatis/config/DataConfig.java
+++ src/main/java/com/study/springmybatis/config/DataConfig.java
@@ -1,5 +1,7 @@
 package com.study.springmybatis.config;
 
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -9,10 +11,22 @@ import javax.sql.DataSource;
 @Configuration
 public class DataConfig {
 
+    @Bean
     public DataSource dataSource(){
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("classpath:datasources/schema.sql")
                 .build();
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource){
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        sqlSessionFactoryBean.setConfiguration(configuration);
+
+        return sqlSessionFactoryBean;
     }
 }
