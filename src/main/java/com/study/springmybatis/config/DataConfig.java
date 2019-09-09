0a28af75747fd32605caea44c5c3b098cd2875de
ZhangQiaofei <18801146258@163.com>
2019-09-09 22:30:17 +0800

    2019/09/09

--- src/main/java/com/study/springmybatis/config/DataConfig.java
+++ src/main/java/com/study/springmybatis/config/DataConfig.java
@@ -1,9 +1,12 @@
 package com.study.springmybatis.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -20,11 +23,28 @@ import java.io.IOException;
 public class DataConfig {
 
     @Bean
+    @Profile("dev")
     public DataSource dataSource(){
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("classpath:database/schema.sql")
+                .addScript("classpath:database/test_data.sql")
                 .build();
+    }
+
+    @Bean
+    @Profile("pro")
+    public DataSource dataSource(@Value("${jdbc.url}") String url,
+                                 @Value("${jdbc.username}") String username,
+                                 @Value("${jdbc.password}") String password){
+        BasicDataSource bs = new BasicDataSource();
+        bs.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        bs.setUrl(url);
+        bs.setUsername(username);
+        bs.setPassword(password);
+        bs.setInitialSize(1);
+
+        return bs;
     }
 
     @Bean
--- src/main/java/com/study/springmybatis/config/DispatcherServletXml.java
+++ src/main/java/com/study/springmybatis/config/DispatcherServletXml.java
@@ -4,6 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -38,5 +39,11 @@ public class DispatcherServletXml implements WebMvcConfigurer {
         messageSource.setBasename("messages");
         messageSource.setCacheSeconds(10);
         return messageSource;
+    }
+
+    //配置解析占位符
+    @Bean
+    public PropertySourcesPlaceholderConfigurer placeholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
